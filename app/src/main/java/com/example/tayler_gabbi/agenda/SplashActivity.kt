2ad2a7ea7ef723f1089d9964517b28926811c7de
montes8 <emontesinos812@gmail.com>
2018-12-21 10:32:35 -0500

    agregando logica al splash

--- app/src/main/java/com/example/tayler_gabbi/agenda/SplashActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/agenda/SplashActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tayler_gabbi.agenda
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 
 
 class SplashActivity : AppCompatActivity() {
@@ -9,5 +13,36 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
+        val anima : Animation
+        val anima2 : Animation
+
+        anima = AnimationUtils.loadAnimation(this, R.anim.animacion)
+        anima2 = AnimationUtils.loadAnimation(this,R.anim.animacion_botton)
+        once.animation=anima
+        doce.animation=anima2
+
+
+        llamarSplash()
+
+
     }
+    fun llamarSplash (){
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep((3*1000).toLong())
+                    val i = Intent(baseContext, LoginActivity::class.java)
+                    startActivity(i)
+                    finish()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
+            }
+        }
+        background.start()
+    }
+
 }
