647c74e8e8ece6f7f5caa1132140290d0f679589
montes8 <emontesinos812@gmail.com>
2018-12-23 20:06:33 -0500

    creando tabla

--- app/src/main/java/com/example/tayler_gabbi/agenda/database/DataBase.kt
+++ app/src/main/java/com/example/tayler_gabbi/agenda/database/DataBase.kt
@@ -1,4 +1,12 @@
 package com.example.tayler_gabbi.agenda.database
 
-class DataBase {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.RoomDatabase
+import com.example.tayler_gabbi.agenda.database.dao.UsuarioDao
+import com.example.tayler_gabbi.agenda.database.model.Usuario
+
+@Database(entities = [Usuario::class],version = 1)
+abstract class DataBase : RoomDatabase() {
+
+    abstract fun usuarioDao() : UsuarioDao
 }
