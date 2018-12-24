4a8ef4ee68b6e387d0142ebf7f182a07981fdbf4
montes8 <emontesinos812@gmail.com>
2018-12-23 19:45:45 -0500

    agregando mas variables a la entidad usuario

--- app/src/main/java/com/example/tayler_gabbi/agenda/database/model/Usuario.kt
+++ app/src/main/java/com/example/tayler_gabbi/agenda/database/model/Usuario.kt
@@ -2,12 +2,22 @@ package com.example.tayler_gabbi.agenda.database.model
 
 class Usuario (
         var idUsu : Long? = null,
-
-        var name : String = "",
-
         var user : String = "",
+        var name : String = "",
+        var lastName :String = "",
+        var mobile : String = "",
+        var password : String = "",
+        var birthdate : String = "",
+        var email : String = "",
+        var address : String = "",
+        var country : String = "",
+        //var photo : String = "",
+        var stydyCenter : String = "",
+        var description : String = ""
 
-        var password : String = ""
+
+
+
 
 
 )
